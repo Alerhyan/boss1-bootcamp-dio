9539c598e2998da641d08531e253b2a651d70953
Alerhyan <alerhyan2007@gmail.com>
2023-11-20 17:05:52 -0300

    primeiro commit

---
+++ boss1/index.js
@@ -0,0 +1,6 @@
+// Criando variaveis com nomes de herois.
+// Criando variaveis com xp de cada Heroi.
+let nomeHeroi = "Prodigy"
+let xpHeroi = 5200
+
+console.log("O Herói de nome " + nomeHeroi + " esta no nivel " + xpHeroi)
